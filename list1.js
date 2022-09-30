a68640a6c8ff009dbf4de493c793d2412cc0d7a6
Nancy Xing <nxing@bowdoin.edu>
2022-09-30 16:06:00 -0400

    Implemented trials with conditional lists.

---
+++ list1.js
@@ -0,0 +1,122 @@
+var list1 = [
+    {
+      "target": "abacus",
+      "prompt": "Instrument for performing calculations by sliding beads along rods or grooves",
+      "prime_given": "ALGORITHM",
+      "prime_condition": "both"
+    },
+    {
+      "target": "Carver",
+      "prompt": "Last name of man credited with inventing peanut butter",
+      "prime_given": "CARLISLE",
+      "prime_condition": "both"
+    },
+    {
+      "target": "Andes",
+      "prompt": "South America's largest mountain range",
+      "prime_given": "ALPS",
+      "prime_condition": "both"
+    },
+    {
+      "target": "anachronism",
+      "prompt": "Something out of keeping with the time in which it exists",
+      "prime_given": "ABNORMALITY",
+      "prime_condition": "both"
+    },
+    {
+      "target": "chameleon",
+      "prompt": "A small lizard with skin that changes color to match its surroundings",
+      "prime_given": "CAMOFLAUGE",
+      "prime_condition": "both"
+    },
+    {
+      "target": "hemorrhage",
+      "prompt": "The escape of blood from vessels, including internal and external bleeding",
+      "prime_given": "HOMEOWNER",
+      "prime_condition": "phonological"
+    },
+    {
+      "target": "Oswald",
+      "prompt": "Last name of JFK's assassin",
+      "prime_given": "OSMOSIS",
+      "prime_condition": "phonological"
+    },
+    {
+      "target": "Nairobi",
+      "prompt": "Capital of Kenya",
+      "prime_given": "NARRATE",
+      "prime_condition": "phonological"
+    },
+    {
+      "target": "Nuremberg",
+      "prompt": "German city for which antisemitic laws were named",
+      "prime_given": "NEUROSURGERY",
+      "prime_condition": "phonological"
+    },
+    {
+      "target": "libel",
+      "prompt": "The illegal act of writing untrue things about someone",
+      "prime_given": "LABEL",
+      "prime_condition": "phonological"
+    },
+    {
+      "target": "Yellowstone",
+      "prompt": "National park in which Old Faithful is located",
+      "prime_given": "GLACIER",
+      "prime_condition": "semantic"
+    },
+    {
+      "target": "Watson",
+      "prompt": "Last name of Sherlock Holmes' assistant and friend",
+      "prime_given": "DOYLE",
+      "prime_condition": "semantic"
+    },
+    {
+      "target": "tic",
+      "prompt": "A habitual spastic motion of particular muscles, especially in the face",
+      "prime_given": "JERK",
+      "prime_condition": "semantic"
+    },
+    {
+      "target": "Seoul",
+      "prompt": "Capital of South Korea",
+      "prime_given": "TOKYO",
+      "prime_condition": "semantic"
+    },
+    {
+      "target": "Wayne",
+      "prompt": "Last name of Batman's secret identity",
+      "prime_given": "KENT",
+      "prime_condition": "semantic"
+    },
+    {
+      "target": "polygamy",
+      "prompt": "The practice of having more than one spouse at the same time",
+      "prime_given": "GEM",
+      "prime_condition": "unrelated"
+    },
+    {
+      "target": "Dante",
+      "prompt": "Italian poet known for writing The\tInferno",
+      "prime_given": "TENNIS",
+      "prime_condition": "unrelated"
+    },
+    {
+      "target": "Garland",
+      "prompt": "Last name of actress who played Dorothy in Wizard of Oz",
+      "prime_given": "WATER",
+      "prime_condition": "unrelated"
+    },
+    {
+      "target": "Helsinki",
+      "prompt": "Capital of Finland",
+      "prime_given": "SHOP",
+      "prime_condition": "unrelated"
+    },
+    {
+      "target": "denigrate",
+      "prompt": "To criticize unfairly; to attack the reputation of",
+      "prime_given": "RHINO",
+      "prime_condition": "unrelated"
+    }
+  ]
---
+++ practice.js
@@ -0,0 +1,32 @@
+var practice_stimuli = [
+    {
+      "target": "Biden",
+      "prompt": "President of the United States of America in 2022",
+      "prime_given": "BYRON",
+      "prime_condition": "practice"
+    },
+    {
+      "target": "Cinderella",
+      "prompt": "Which Disney princess lost her shoe at the ball?",
+      "prime_given": "ARIEL",
+      "prime_condition": "practice"
+    },
+    {
+      "target": "temperature",
+      "prompt": "What does a thermometer measure?",
+      "prime_given": "FROG",
+      "prime_condition": "practice"
+    },
+    {
+      "target": "mercury",
+      "prompt": "Which planet is closest to the sun?",
+      "prime_given": "MERCHANT",
+      "prime_condition": "practice"
+    },
+    {
+      "target": "Bell",
+      "prompt": "Last name of the man who invented the telephone?",
+      "prime_given": "BOHR",
+      "prime_condition": "practice"
+    }
+  ]
